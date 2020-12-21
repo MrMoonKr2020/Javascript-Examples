6774fbf986abb330877eab5849074c09b271d7a8
MrMoonKr2020 <cycherMoon@naver.com>
2020-12-21 20:23:28 +0900

    코드 리팩토링

--- Chapter01/C0103/sketch.js
+++ Chapter01/C0103/sketch.js
@@ -5,8 +5,8 @@
 
 // Mutual repulsion, with optional gravity
 
-var myParticles = [];
-var nParticles = 250;
+var g_Particles         = [];
+var g_NumParticles      = 250;
 var maxRadius = 120;
 var clicked = false;
 var margin = 20;
@@ -20,22 +20,22 @@ function setup()
     createCanvas( 800, 600 );
     noStroke();
 
-    for (var i = 0; i < nParticles; i++) {
+    for (var i = 0; i < g_NumParticles; i++) {
         var radius = random(0, maxRadius);
         var angle = random(0, TWO_PI);
         var rx = cos(angle) * radius + width / 2;
         var ry = sin(angle) * radius + height / 2;
         // var rx = random(width/2 - radius, width/2 + radius);
         // var ry = random(height/2 - radius, height/2 + radius);
-        myParticles[i] = new Particle(random(5, 7));
-        myParticles[i].set(rx, ry);
+        g_Particles[i] = new Particle(random(5, 7));
+        g_Particles[i].set(rx, ry);
     }
 }
 
 function keyPressed() {
-    for (var i = 0; i < myParticles.length; i++) {
-        myParticles[i].px = random(width);
-        myParticles[i].py = random(height);
+    for (var i = 0; i < g_Particles.length; i++) {
+        g_Particles[i].px = random(width);
+        g_Particles[i].py = random(height);
     }
 }
 
@@ -56,13 +56,13 @@ function draw()
     var mutualRepulsionAmount = 0.5;
     var mouseMutualRepulsionAmount = 500;
 
-    for (var i = 0; i < myParticles.length; i++) {
-        var ithParticle = myParticles[i];
+    for (var i = 0; i < g_Particles.length; i++) {
+        var ithParticle = g_Particles[i];
         var px = ithParticle.px;
         var py = ithParticle.py;
 
         for (var j = 0; j < i; j++) {
-            var jthParticle = myParticles[j];
+            var jthParticle = g_Particles[j];
             var qx = jthParticle.px;
             var qy = jthParticle.py;
 
@@ -116,19 +116,19 @@ function draw()
         } else {
         }
     }
-    for (var i = 0; i < myParticles.length; i++) {
-        myParticles[i].bPeriodicBoundaries = false;
-        myParticles[i].bElasticBoundaries = true;
+    for (var i = 0; i < g_Particles.length; i++) {
+        g_Particles[i].bPeriodicBoundaries = false;
+        g_Particles[i].bElasticBoundaries = true;
 
         // var distanceFromCenter = getDistance(width/2, height/2, myParticles[i].px, myParticles[i].py)
         // if(distanceFromCenter <= maxRadius ){
         //   myParticles[i].update();
         // } // update all locations
-        myParticles[i].update();
+        g_Particles[i].update();
     }
 
-    for (var i = 0; i < myParticles.length; i++) {
-        myParticles[i].render(); // draw all particles
+    for (var i = 0; i < g_Particles.length; i++) {
+        g_Particles[i].render(); // draw all particles
     }
 
     fill(200, 200, 200);
@@ -146,7 +146,7 @@ function draw()
 
 //==========================================================
 //==========================================================
-function Particle(size) {
+function Particle( size ) {
     this.px = 0;
     this.py = 0;
     this.vx = 0;
@@ -158,7 +158,8 @@ function Particle(size) {
     this.bElasticBoundaries = true;
     this.size = size;
 
-    this.set = function (x, y) {
+    this.set = function (x, y) 
+    {
         this.px = x;
         this.py = y;
         this.vx = 0;
@@ -174,11 +175,14 @@ function Particle(size) {
         this.vy += ay;
     };
 
-    this.update = function () {
+    this.update = function() 
+    {
         this.vx *= this.damping;
         this.vy *= this.damping;
+
         this.limitVelocities();
         this.handleBoundaries();
+
         this.px += this.vx;
         this.py += this.vy;
     };
@@ -236,24 +240,31 @@ function Particle(size) {
             if (this.py < 0) this.vy = -this.vy;
         }
     };
-    this.render = function (size) {
+
+    this.render = function (size) 
+    {
         var distance = getDistance(mouseX, mouseY, this.px, this.py);
         var factor = map(distance, 0, 75, 3, 1);
-        if (distance >= 75) {
+        if (distance >= 75) 
+        {
             var size = this.size + log(abs(this.vx) * 50);
-        } else {
+        } 
+        else 
+        {
             clicked = true;
             var size = (this.size + log(abs(this.vx) * 50)) * factor;
         }
-        var opacity = map(size, 0, 40, 100, 255);
 
+        var opacity = map( size, 0, 40, 100, 255 );
         // var opacity = map(size, 0, 100, 3, 1)
-        fill(132, 0, 248, opacity);
-
-        ellipse(this.px, this.py, size, size);
+        fill( 132, 0, 248, opacity );
+        ellipse( this.px, this.py, size, size );
     };
 }
 
-function getDistance(x1, y1, x2, y2) {
-    return sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
+function getDistance( x1, y1, x2, y2 ) 
+{
+    return sqrt( ( x1 - x2 ) ** 2 + ( y1 - y2 ) ** 2 );
 }
+
+
